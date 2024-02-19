cb1110bad3574d7ed00dcebdadf29397ea2c0005
Plamena <yomena37@gmail.com>
2024-02-19 23:08:48 +0200

    Code cleanup

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -61,7 +61,7 @@ const ComputersCanvas = () => {
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
+      gl={{ preserveDrawingBuffer: true, alpha: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         {/* OrbitControls for interactive camera control */}
